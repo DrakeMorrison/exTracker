cd6e22073d45516df881dee74165c2834a5d520c
Drake Morrison <drakermorrison@gmail.com>
2018-05-04 13:34:26 -0500

    added filterByTime functions and reset uset button

--- javascripts/events.js
+++ javascripts/events.js
@@ -3,12 +3,34 @@ const filterSearchBar = (e) => {
 };
 
 const filterTime = (e) => {
-  console.error('filterTime', e);
+  const target = e.target.id;
+  if (target === 'morning-btn') {
+    $('p:contains("Morning")').closest('div.panel').hide();
+    $('#reset-btn').closest('div').removeClass('hide');
+
+  } else if (target === 'afternoon-btn') {
+    $('p:contains("Afternoon")').closest('div.panel').hide();
+    $('#reset-btn').closest('div').removeClass('hide');
+
+  } else if (target === 'evening-btn') {
+    $('p:contains("Evening")').closest('div.panel').hide();
+    $('#reset-btn').closest('div').removeClass('hide');
+
+  } else if (target === 'after-dark-btn') {
+    $('p:contains("After Dark")').closest('div.panel').hide();
+    $('#reset-btn').closest('div').removeClass('hide');
+  }
+};
+
+const resetBtn = () => {
+  $('div.panel').show();
+  $('#reset-btn').closest('div').addClass('hide');
 };
 
 const addEventListeners = () => {
   $('#searchBar').on('keydown', filterSearchBar);
   $('.time-btn').on('click', filterTime);
+  $('#reset-btn').on('click', resetBtn);
 };
 
 module.exports = addEventListeners;
